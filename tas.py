c426c740bbc59a600cb81f6b73f585fe7ec52dec
Cengizhan Terzioğlu <cengizhan@ug.bilkent.edu.tr>
2024-08-16 14:41:53 +0300

    Elements, points and few necessary function signatures without parameters

---
+++ tas.py
@@ -0,0 +1,39 @@
+# These are the 9 playable elements for this game. They replace rock, paper and scissors.
+elements = ["fire", "water", "wind", "electric", "nature", "earth", "metal", "light", "dark"]
+
+# A 1D list for keeping the points between each element
+# Suppose i and j are indices of 2 elements from the elements list:
+# pointTable[9*i + j] retrieves how many points i will get against j.
+pointTable = (0, -5, -2, 1, 5, -3, 3, -1, 2,
+              5, 0, -1, -4, -5, 3, 2, 2, -2,
+              2, 1, 0, 5, -1, -3, -5, 3, -2,
+              -1, 4, -5, 0, 2, -5, 5, -3, 3,
+              -5, 5, 1, -2, 0, 4, -3, 4, -4,
+              3, -3, 3, 5, -4, 0, -2, 2, -4,
+              -3, -2, 5, -5, 3, 2, 0, -2, 2,
+              1, -2, -3, 3, -4, -2, 2, 0, 5,
+              -2, 2, 2, -3, 4, 4, -2, -5, 0)
+
+def tacticalArcaneShowdown():
+    pass
+
+def gameStartPrep():
+    pass
+
+def roundUpdate():
+    pass
+
+def matchUpdate():
+    pass
+
+def suggestThreeElements():
+    pass
+
+def displayWinningOdds():
+    pass
+
+def activatePowerUp():
+    pass
+
+def chooseFinalElement():
+    pass
